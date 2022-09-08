6aa9916e6715d37c4410137b3f2bb223cda2c5e4
galileuveloso <galileu.veloso@hotmail.com>
2022-09-08 14:55:55 -0300

    get por Id e get por Id Cliente

--- src/main/java/com/bantads/conta/bantadsconta/BantadsContaApplication.java
+++ src/main/java/com/bantads/conta/bantadsconta/BantadsContaApplication.java
@@ -1,8 +1,10 @@
 package com.bantads.conta.bantadsconta;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @EnableRabbit
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class BantadsContaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BantadsContaApplication.class, args);
+	}
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
--- src/main/java/com/bantads/conta/bantadsconta/controller/ContaController.java
+++ src/main/java/com/bantads/conta/bantadsconta/controller/ContaController.java
@@ -6,19 +6,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.bantads.conta.bantadsconta.DTOs.ContaDTO;
 import com.bantads.conta.bantadsconta.DTOs.ContaResponseDTO;
+import com.bantads.conta.bantadsconta.data.R.ContaRRepository;
 import com.bantads.conta.bantadsconta.model.R.ContaR;
-import com.bantads.conta.bantadsconta.services.SenderConta;
 import com.bantads.conta.bantadsconta.services.SenderTeste;
 
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,7 +27,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class ContaController {
 	
 	@Autowired
+    private ModelMapper mapper;
+	
+	@Autowired
     private SenderTeste senderTeste;
+	
+	@Autowired
+	private ContaRRepository contaRRepository;
 	
     @GetMapping("/health")
     public String health() {
@@ -46,4 +50,41 @@ public class ContaController {
     		return ResponseEntity.status(500).build();
     	}
     }
+    
+    @GetMapping("/obter-idcliente/{idCliente}")
+    public ResponseEntity<ContaResponseDTO> obterPorIdCliente(@PathVariable UUID idCliente){
+    	try {
+    		ContaR conta = contaRRepository.findByIdCliente(idCliente);
+        	
+        	if(conta != null) {
+        		ContaResponseDTO response = mapper.map(conta, ContaResponseDTO.class);
+            	return ResponseEntity.ok().body(response);
+        	} else {
+        		return ResponseEntity.ok().body(null);
+        	}
+    	} catch(Exception ex) {
+    		ex.printStackTrace();
+    		return ResponseEntity.status(500).build();
+    	}
+    }
+    
+    @GetMapping("/obter/{id}")
+    public ResponseEntity<ContaResponseDTO> obterPorId(@PathVariable UUID id){
+    	try {
+    		Optional<ContaR> contaOpt = contaRRepository.findById(id);
+
+    		ContaR conta = contaOpt.get();
+    		
+        	if(conta != null) {
+        		ContaResponseDTO response = mapper.map(conta, ContaResponseDTO.class);
+            	return ResponseEntity.ok().body(response);
+        	} else {
+        		return ResponseEntity.ok().body(null);
+        	}
+    	} catch(Exception ex) {
+    		ex.printStackTrace();
+    		return ResponseEntity.status(500).build();
+    	}
+    }
+    
 }
