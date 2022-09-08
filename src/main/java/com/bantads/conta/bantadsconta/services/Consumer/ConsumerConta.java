3939764afb2dc4c32f7a7eef9ab30f72f5c56071
noh4nsen <noh4nsen@gmail.com>
2022-09-08 15:48:03 -0300

    fix: correcao em ConsumerConta

--- src/main/java/com/bantads/conta/bantadsconta/services/Consumer/ConsumerConta.java
+++ src/main/java/com/bantads/conta/bantadsconta/services/Consumer/ConsumerConta.java
@@ -1,8 +1,6 @@
 package com.bantads.conta.bantadsconta.services.Consumer;
 
 import java.math.BigDecimal;
-import java.util.UUID;
-
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -25,36 +23,38 @@ public class ConsumerConta {
 
     @Autowired
     private GerenteContaRepository gerenteContaRepository;
-    
-	@Autowired
+
+    @Autowired
     private ContaRRepository contaRepositoryR;
-	
-	@Autowired
+
+    @Autowired
     private ContaCRepository contaRepositoryCUD;
-	
-	@Autowired
+
+    @Autowired
     private SenderConta senderCliente;
-	
-	@RabbitListener(queues = "conta-inserir")
+
+    @RabbitListener(queues = "conta-inserir")
     public void receive(@Payload String json) {
         try {
-        	ContaDTO contaDTO = objectMapper.readValue(json, ContaDTO.class);
-        	
-        	ContaC conta = new ContaC(contaDTO);
+            ContaDTO contaDTO = objectMapper.readValue(json, ContaDTO.class);
+
+            ContaC conta = new ContaC(contaDTO);
             conta.setNumero(contaRepositoryCUD.maxNumero() + 1);
             contaRepositoryCUD.save(conta);
-        	senderCliente.sendInserirRead(conta);
+            senderCliente.sendInserirRead(conta);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-	
-	@RabbitListener(queues = "conta-inserir-read")
+
+    @RabbitListener(queues = "conta-inserir-read")
     public void receiveRead(@Payload String json) {
         try {
-        	ContaR conta = objectMapper.readValue(json, ContaR.class);
-        	contaRepositoryR.save(conta);
+            ContaR conta = objectMapper.readValue(json, ContaR.class);
+            contaRepositoryR.save(conta);
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
+}
